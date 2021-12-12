d8ff4ce4dc72d6904c26d2a4f099fe93f647a354
mchienbk <minhchien2031995@gmail.com>
2021-12-12 17:20:05 +0000

    Update source code for lgt328p

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,34 @@
-#include <Arduino.h>
+/*
+ * Created on Tue Feb 18 2020
+ *
+ * Copyright (c) 2020 - Your Company
+ */
+
 /* Pin layout
-      _________
-RST - |       | - SCL
-D2  - |       | - SDA
-D3~ - |       | -  A1
-VCC - |       | -  A0
-GND - |       | - D13 (LED)
-D5~ - |       | - D12
-D6~ - |       | - D11
-D7  - |       | - ~D9
-GND - |       | - GND
-RAW - |_______| - VCC     */
+        _________
+  RST - |       | - SCL
+  D2  - |       | - SDA
+  D3~ - |       | -  A1
+  VCC - |       | -  A0
+  GND - |       | - D13 (LED)
+  D5~ - |       | - D12
+  D6~ - |       | - D11
+  D7  - |       | - ~D9
+  GND - |       | - GND
+  RAW - |_______| - VCC
 
+  - 2 input for limmit switch (1)
+  - 2 input for limmit switch (2)
+  - 2 output for limmit motor (1)
+  - 2 output for limmit motor (2)
+  - 1 out pur for led
+ */
 
-// 2 input for limmit switch (1)
-// 2 input for limmit switch (2)
-// 2 output for limmit motor (1)
-// 2 output for limmit motor (2)
-// 1 out pur for led
-
+#include <Arduino.h>
 #define X_DIR    D3  // x -axis control
 #define X_STP    D2  // x -axis direction 
 #define X_HOME   D12 // x -axis control
 #define X_LIMIT  D5  // x -axis direction 
-
 #define Y_DIR    D6  // y -axis control
 #define Y_STP    D7  // y -axis direction
 #define Y_HOME   D11 // x -axis control
