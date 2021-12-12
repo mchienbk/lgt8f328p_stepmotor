3787546f098ac6adb827579600d6a7a3ebcb3e98
mchienbk <minhchien2031995@gmail.com>
2021-12-12 17:25:41 +0000

    Update source code

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,7 @@
  */
 
 #include <Arduino.h>
-#define X_DIR    D3  // x -axis control
-#define X_STP    D2  // x -axis direction 
-#define X_HOME   D12 // x -axis control
-#define X_LIMIT  D5  // x -axis direction 
-#define Y_DIR    D6  // y -axis control
-#define Y_STP    D7  // y -axis direction
-#define Y_HOME   D11 // x -axis control
-#define Y_LIMIT  D9  // x -axis direction 
+#include <main.h>
 
 void GPIO_Init(void);
 void Led_ON(void);
